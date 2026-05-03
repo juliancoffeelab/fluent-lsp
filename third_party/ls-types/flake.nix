c77ca0db32a9ced8270485b4bfc260b8ed245e76
juliancoffee <lightdarkdaughter@gmail.com>
2026-05-03 08:42:38 +0000

    vendor: add ls-types snippet edit support

---
+++ third_party/ls-types/flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+
+      forAllSystems = genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllPkgs = function: forAllSystems (system: function pkgs.${system});
+
+      pkgs = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        }
+      );
+    in
+    {
+      formatter = forAllPkgs (pkgs: pkgs.nixfmt-tree);
+
+      devShells = forAllPkgs (
+        pkgs:
+        let
+          file-rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rust-toolchain = file-rust-toolchain.override { extensions = [ "rust-analyzer" ]; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              pkg-config
+              rust-toolchain
+            ];
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+        }
+      );
+    };
+}
